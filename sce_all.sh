cac160fcd82d726a6278597776c2840a10f76e38
bluenote-1577 <jimshawster@gmail.com>
2022-12-20 21:30:40 -0800

    removed old cmd

--- sce_all.sh
+++ sce_all.sh
@@ -1,6 +1,5 @@
 ./target/debug/sce_aligner refs/good_refs/covid.fa cov_reads_100000.fastq > cov_results.txt
 ./target/debug/sce_aligner refs/good_refs/e.coli-K12.fasta ecoli_reads_100000.fastq  > ecoli_results.txt
-./target/debug/sce_aligner refs/good_refs/aspergillus.fna fungi_reads_500000.fastq  > fungi_results.txt
 ./target/debug/sce_aligner refs/good_refs/rice_fungus.fa ./rice_fungus_100000.fastq > rice_fungus_results.txt
 ./target/debug/sce_aligner refs/good_refs/drosophila.fna fly_reads_100000.fastq  > fly_results.txt
 ./target/debug/sce_aligner ~/scratch/REFERENCES/chm13.draft_v1.1.fasta human_reads_500000.fastq > human_results.txt
